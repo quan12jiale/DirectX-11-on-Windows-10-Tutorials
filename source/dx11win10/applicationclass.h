e67014f6b8b035c3a279b46e9e40dfb5bcd37789
quanjiale <quanjiale@kuaishou.com>
2025-07-14 14:11:56 +0800

    Tutorial 3: Initializing DirectX 11

--- source/dx11win10/applicationclass.h
+++ source/dx11win10/applicationclass.h
@@ -10,6 +10,10 @@
 //////////////
 #include <windows.h>
 
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "d3dclass.h"
 
 /////////////
 // GLOBALS //
@@ -37,7 +41,7 @@ private:
 	bool Render();
 
 private:
-
+	D3DClass* m_Direct3D;
 };
 
 #endif
