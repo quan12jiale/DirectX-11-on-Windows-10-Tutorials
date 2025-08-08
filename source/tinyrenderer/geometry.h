06d064c9791ae0d38f993ad60ee4c568a079f3dd
quanjiale <quanjiale@kuaishou.com>
2025-08-08 20:00:11 +0800

    tinyrenderer：提交mat类模板

--- source/tinyrenderer/geometry.h
+++ source/tinyrenderer/geometry.h
@@ -28,6 +28,33 @@ template<int n> vec<n> operator*(const vec<n>& lhs, const double rhs) {
 	return ret;
 }
 
+template<int n> vec<n> operator+(const vec<n>& lhs, const vec<n>& rhs) {
+	vec<n> ret = lhs;
+	for (int i = 0; i < n; i++)
+	{
+		ret[i] += rhs[i];
+	}
+	return ret;
+}
+
+template<int n> vec<n> operator-(const vec<n>& lhs, const vec<n>& rhs) {
+	vec<n> ret = lhs;
+	for (int i = 0; i < n; i++)
+	{
+		ret[i] -= rhs[i];
+	}
+	return ret;
+}
+
+template<int n> vec<n> operator/(const vec<n>& lhs, const double rhs) {
+	vec<n> ret = lhs;
+	for (int i = 0; i < n; i++)
+	{
+		ret[i] /= rhs;
+	}
+	return ret;
+}
+
 template<> struct vec<3> 
 {
 	double x = 0, y = 0, z = 0;
@@ -66,3 +93,24 @@ template<int nrows, int ncols> struct mat
 	vec<ncols>& operator[](const int idx)	   { assert(idx >= 0 && idx < nrows); return rows[idx]; }
 	vec<ncols> operator[](const int idx) const { assert(idx >= 0 && idx < nrows); return rows[idx]; }
 };
+
+/*
+这是一个模板函数，重载了乘法运算符*，用于计算两个矩阵的乘积。
+参数含义
+R1: 左矩阵的行数
+C1: 左矩阵的列数 = 右矩阵的行数
+C2: 右矩阵的列数
+*/
+template<int R1, int C1, int C2> mat<R1, C2> operator*(const mat<R1, C1>& lhs, const mat<C1, C2>& rhs)
+{
+	mat<R1, C2> result;
+	for (int i = 0; i < R1; i++)
+	{
+		for (int j = 0; j < C2; j++) {
+			for (int k = 0; k < C1; k++) {
+				result[i][j] += lhs[i][k] * rhs[k][j];
+			}
+		}
+	}
+	return result;
+}
--- source/tinyrenderer/our_gl.cpp
+++ source/tinyrenderer/our_gl.cpp
@@ -2,7 +2,21 @@
 
 mat<4, 4> Viewport;
 mat<4, 4> Projection;
+mat<4, 4> ModelView;
 
+/*
+视口变换函数
+功能: 将标准化设备坐标(NDC)转换为屏幕坐标
+
+变换过程:
+输入: NDC坐标范围 [-1, 1]
+输出: 屏幕像素坐标 [x, x+w] × [y, y+h]
+
+视口变换数学推导
+对于NDC坐标(x_ndc, y_ndc)在范围[-1, 1]：
+缩放: 从[-1,1]映射到[0, w]和[0, h]
+平移: 移动到视口起始位置(x, y)
+*/
 void viewport(const int x, const int y, const int w, const int h)
 {
 	Viewport = { {{w / 2.0,0,0,x + w / 2.0},
@@ -12,6 +26,7 @@ void viewport(const int x, const int y, const int w, const int h)
 }
 
 /*
+投影变换函数
 这是一个简化的透视投影：
 
 保持X坐标不变
@@ -29,6 +44,26 @@ void projection(const double coeff)
 		{0,0,-1 / coeff,0}} };
 }
 
+/*
+lookat函数用于构建3D相机的视图变换矩阵（ModelView矩阵），将世界坐标系转换到相机坐标系。
+ModelView = 旋转矩阵 * 平移矩阵
+1、旋转矩阵（坐标轴对齐）
+这个矩阵将世界坐标系对齐到相机坐标系
+2、平移矩阵（移动到原点）
+将相机位置移动到坐标原点。
+*/
 void lookat(const vec3 eye, const vec3 center, const vec3 up)
 {
+	const vec3 z = normalized(center - eye);
+	const vec3 x = normalized(cross(up, z));
+	const vec3 y = normalized(cross(z, x));
+	ModelView = mat<4, 4>{ {{x.x,x.y,x.z,0}, 
+		{y.x,y.y,y.z,0}, 
+		{z.x,z.y,z.z,0}, 
+		{0,0,0,1}} } 
+		* 
+		mat<4, 4>{ {{1, 0, 0, -eye.x},
+			{ 0,1, 0,  -eye.y },
+			{ 0, 0, 1, -eye.z },
+			{ 0, 0, 0, 1 }} };
 }
