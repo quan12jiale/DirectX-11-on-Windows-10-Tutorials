94d213b2ac267cf74acdeec62daa84ed70d97edd
quanjiale <quanjiale@kuaishou.com>
2025-08-21 17:15:42 +0800

    learnopengl\1.getting_started\7.1.camera_circle

---
+++ source/openglwin10/learnopengl/1.getting_started/7.1.camera_circle/camera_circle.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "applicationclass.h"
+#include <QVector3D>
+
+class Shader;
+// https://github.com/JoeyDeVries/LearnOpenGL/tree/master
+class CameraCircle : public ApplicationClass {
+public:
+	bool Initialize(int screenWidth, int screenHeight, HWND hwnd) override;
+	void Shutdown() override;
+private:
+	bool Frame() override;
+private:
+	OpenGLClass* m_OpenGL;
+	Shader* ourShader;
+	unsigned int VBO, VAO;
+	unsigned int instanceVBO;
+	unsigned int texture1, texture2;
+	QVector<QVector3D> cubePositions;
+	qint64 init_timestamp;
+};
